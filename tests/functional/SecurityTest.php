97d5efe083c8ee8f600531c0ff37b101835a73c7
Micha Alt <micha@haus23.net>
2020-02-24 22:01:55 +0100

    Bug: Migration feature not secured. See #6.

---
+++ tests/functional/SecurityTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Functional\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityTest extends WebTestCase
+{
+    public function testMigrationFeatureIsSecured()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/migration');
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+}
