f7e4678d0ee5e4586dedb0e68e638954b18b4f7b
Micha Alt <micha@haus23.net>
2020-02-27 16:34:41 +0100

    Linking to the backend.

--- src/Controller/Backend/DefaultController.php
+++ src/Controller/Backend/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/backend/default", name="backend_default")
+     * @Route("/backend", name="backend")
      */
     public function index()
     {
